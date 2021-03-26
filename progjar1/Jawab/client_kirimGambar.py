be877cdf55042712a860648244952e992f7295cd
amlpm <ameliapuji2000@gmail.com>
2021-03-27 00:01:21 +0800

    Third Commit - Indentend Block

--- progjar1/Jawab/client_kirimGambar.py
+++ progjar1/Jawab/client_kirimGambar.py
@@ -4,14 +4,14 @@ import socket
 ip = ['192.168.122.25', '192.168.122.36']
 
 for x in range(1,2):
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Create a TCP/IP socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Connect the socket to the port where the server is listening
-server_address = (ip[x], 10000)
-print(f"Connecting to {server_address}")
-sock.connect(server_address)
-print(f"Connected to {server_address}")
+    # Connect the socket to the port where the server is listening
+    server_address = (ip[x], 10000)
+    print(f"Connecting to {server_address}")
+    sock.connect(server_address)
+    print(f"Connected to {server_address}")
 
 try:
 
